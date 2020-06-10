3079f8eb78e55d7be6df6ef3c745ddb02ade0be4
Augustinas Cerniauskas <augis1305@gmail.com>
2020-06-10 21:02:07 +0100

    added presentation

--- src/views/Movies.vue
+++ src/views/Movies.vue
@@ -1,6 +1,9 @@
 <template >
-  <div class="container">
-    <table class="table table-dark" >
+  <div class="container" >
+    <div class="table">
+      <input type="text" v-model="season" @keyup="getResults(season)">
+    </div>
+    <table class="table table-dark">
       <thead>
         <tr>
           <th scope="col">Episode</th>
@@ -9,8 +12,8 @@
           <th scope="col">Vote</th>
         </tr>
       </thead>
-      <tbody>
-        <tr v-for="allEpisodes in info" :key="allEpisodes">
+      <tbody >
+        <tr v-for="allEpisodes in results" :key="allEpisodes"> 
           <th scope="row">{{allEpisodes.episode_number}}</th>
           <td>{{allEpisodes.name}}</td>
           <td>{{allEpisodes.overview}}</td>
@@ -26,23 +29,26 @@ import axios from "axios";
 export default {
   data() {
     return {
-      info: null
+      results: "",
+      season: ""
     };
   },
-  mounted() {
-    axios
-      .get(
-        "https://api.themoviedb.org/3/tv/1399/season/1?api_key=a80f05423b30ed9d18d929aa01fc2a03&language=en-US"
-      )
-      .then(response => {
-        this.info = response.data.episodes;
-        console.log(this.info);
-      })
-      .catch(error => {
-        console.log(error);
-        this.errored = true;
-      })
-      .finally(() => (this.loading = false));
+  methods: {
+    getResults: function(season) {
+      axios
+        .get(
+          "https://api.themoviedb.org/3/tv/1399/season/" + season + "?api_key=a80f05423b30ed9d18d929aa01fc2a03&language=en-US"
+        )
+        .then(response => {
+          this.results = response.data.episodes;
+          console.log(this.results);
+        })
+        .catch(error => {
+          console.log(error);
+          this.errored = true;
+        })
+        .finally(() => (this.loading = false));
+    }
   }
 };
 </script>
