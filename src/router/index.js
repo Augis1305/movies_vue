14382e1f0f8e8dd940ab6a2aa90e3a384a1959a9
Augustinas Cerniauskas <augustinas.cer@vsweb.visitscotland.net>
2020-07-06 18:31:35 +0100

    something

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,27 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import Movies from "../views/Movies.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-  }
+    {
+        path: "/",
+        name: "Home",
+    },
+    {
+        path: "/seasons",
+        name: "seasons",
+    },
+    {
+        path: "/episode",
+        name: "Episode",
+        component: Movies
+    }
 ];
 
 const router = new VueRouter({
-  routes
+    routes
 });
 
 export default router;
---
+++ src/views/Seasons.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="containter">
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
