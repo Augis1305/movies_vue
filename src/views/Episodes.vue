b9b1908585d16513c393e271c0d068716d9013d2
Augustinas Cerniauskas <augustinas.cer@vsweb.visitscotland.net>
2020-07-10 08:51:20 +0100

    format files

--- src/views/Episodes.vue
+++ src/views/Episodes.vue
@@ -1,60 +1,63 @@
 <template >
-  <div class="container">
-    
-    <div class="table">
-      <div class="col-6">
-        <input class="form-control" type="text" v-model="season" @click="getResults(season)" />
-
-      </div>
+    <div class="container">
+        <div class="table">
+            <div class="col-6">
+                <input
+                    class="form-control"
+                    type="text"
+                    v-model="season"
+                    @keydown="getResults(season)"
+                />
+            </div>
+        </div>
+        <table class="table table-dark">
+            <thead>
+                <tr>
+                    <th scope="col">Episode</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Overview</th>
+                    <th scope="col">Vote</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="allEpisodes in results" :key="allEpisodes">
+                    <th scope="row">{{allEpisodes.episode_number}}</th>
+                    <td>{{allEpisodes.name}}</td>
+                    <td>{{allEpisodes.overview}}</td>
+                    <td>{{allEpisodes.vote_average}}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
-    <table class="table table-dark">
-      <thead>
-        <tr>
-          <th scope="col">Episode</th>
-          <th scope="col">Name</th>
-          <th scope="col">Overview</th>
-          <th scope="col">Vote</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="allEpisodes in results" :key="allEpisodes">
-          <th scope="row">{{allEpisodes.episode_number}}</th>
-          <td>{{allEpisodes.name}}</td>
-          <td>{{allEpisodes.overview}}</td>
-          <td>{{allEpisodes.vote_average}}</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
 </template>
 
 <script>
 import axios from "axios";
 export default {
-  data() {
-    return {
-      results: "",
-      season: ""
-    };
-  },
-  methods: {
-    getResults: (season) => {
-      axios
-        .get(
-          "https://api.themoviedb.org/3/tv/1399/season/" +
-            season +
-            "?api_key=a80f05423b30ed9d18d929aa01fc2a03&language=en-US"
-        )
-        .then(response => {
-          this.results = response.data.episodes;
-          console.log(this.results);
-        })
-        .catch(error => {
-          console.log(error);
-          this.errored = true;
-        })
-        .finally(() => (this.loading = false));
+    data() {
+        return {
+            results: "",
+            season: ""
+        };
+    },
+    methods: {
+        getResults: season => {
+            axios
+                .get(
+                    "https://api.themoviedb.org/3/tv/1399/season/" +
+                        season +
+                        "?api_key=a80f05423b30ed9d18d929aa01fc2a03&language=en-US"
+                )
+                .then(response => {
+                    this.results = response.data.episodes;
+                    console.log(this.results);
+                })
+                .catch(error => {
+                    console.log(error);
+                    this.errored = true;
+                })
+                .finally(() => (this.loading = false));
+        }
     }
-  }
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,52 +1,53 @@
 <template>
-  <div class="container">
-    <div class="row justify-content-md-center">
-      <div class="col-md-5">
-        <h2 class="text-center">Popular Movies</h2>
-        <div class="col-md-auto">
-          <div
-            id="carouselExampleIndicators"
-            class="carousel slide"
-            data-ride="carousel"
-            data-interval="1400"
-          >
-            <div class="carousel-inner">
-              <div
-                class="carousel-item"
-                v-for="(movie, index) in popularMoviesFiltered"
-                :key="index"
-                :class="{ active: index==0}"
-              >
-                <img
-                  class="img-fluid"
-                  :src="'https://image.tmdb.org/t/p/w500' + movie.poster_path"
-                  alt
-                />
-              </div>
+    <div class="container">
+        <div class="row justify-content-md-center">
+            <div class="col-md-5">
+                <h2 class="text-center">Popular Movies</h2>
+                <div class="col-md-auto">
+                    <div
+                        id="carouselExampleIndicators"
+                        class="carousel slide"
+                        data-ride="carousel"
+                        data-interval="1400"
+                    >
+                        <div class="carousel-inner">
+                            <div
+                                class="carousel-item"
+                                v-for="(movie, index) in popularMoviesFiltered"
+                                :key="index"
+                                :class="{ active: index==0}"
+                                @click="loadMovie(movie.id)"
+                            >
+                                <img
+                                    class="img-fluid"
+                                    :src="'https://image.tmdb.org/t/p/w500' + movie.poster_path"
+                                    alt
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <a
+                        class="carousel-control-prev"
+                        href="#carouselExampleIndicators"
+                        role="button"
+                        data-slide="prev"
+                    >
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a
+                        class="carousel-control-next"
+                        href="#carouselExampleIndicators"
+                        role="button col"
+                        data-slide="next"
+                    >
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
             </div>
-          </div>
-          <a
-            class="carousel-control-prev"
-            href="#carouselExampleIndicators"
-            role="button"
-            data-slide="prev"
-          >
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-          </a>
-          <a
-            class="carousel-control-next"
-            href="#carouselExampleIndicators"
-            role="button col"
-            data-slide="next"
-          >
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-          </a>
         </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -54,58 +55,58 @@ import { HomeHttp } from "../resources/resources";
 import _ from "lodash";
 
 export default {
-  name: "Home",
-  data() {
-    return {
-      upcomingMovies: [],
-      popularMovies: [],
-      numberOfItems: 10,
-      sliderInit: false,
-      message: ""
-    };
-  },
-  props: ["sliderOptions", "posterPath"],
-  computed: {
-    upcomingMoviesFiltered() {
-      return _.sampleSize(this.upcomingMovies, this.numberOfItems);
+    name: "Home",
+    data() {
+        return {
+            upcomingMovies: [],
+            popularMovies: [],
+            numberOfItems: 10,
+            sliderInit: false,
+            message: ""
+        };
     },
-    popularMoviesFiltered() {
-      return _.sampleSize(this.popularMovies, this.numberOfItems);
-    }
-  },
-  methods: {
-    fetchData() {
-      let self = this;
-
-      HomeHttp.getUpcomingMovies().then(movies => {
-        this.upcomingMovies = movies.data.results;
-
-        HomeHttp.getPopularMovies().then(
-          movies => {
-            this.popularMovies = movies.data.results;
-            self.$emit("loadingEnd");
-            this.sliderInit = true;
-          },
-          error => {
-            console.log(error);
-          }
-        );
-      });
+    props: ["sliderOptions", "posterPath"],
+    computed: {
+        upcomingMoviesFiltered() {
+            return _.sampleSize(this.upcomingMovies, this.numberOfItems);
+        },
+        popularMoviesFiltered() {
+            return _.sampleSize(this.popularMovies, this.numberOfItems);
+        }
     },
-    loadMovie(id) {
-      this.$router.push({ name: "movie", params: { id: id } });
+    methods: {
+        fetchData() {
+            let self = this;
+
+            HomeHttp.getUpcomingMovies().then(movies => {
+                this.upcomingMovies = movies.data.results;
+
+                HomeHttp.getPopularMovies().then(
+                    movies => {
+                        this.popularMovies = movies.data.results;
+                        self.$emit("loadingEnd");
+                        this.sliderInit = true;
+                    },
+                    error => {
+                        console.log(error);
+                    }
+                );
+            });
+        },
+        loadMovie(id) {
+            this.$router.push({ name: "movie", params: { id: id } });
+        }
+    },
+    created() {
+        this.$emit("resetBg");
+        this.$emit("loadingStart");
+        this.fetchData();
     }
-  },
-  created() {
-    this.$emit("resetBg");
-    this.$emit("loadingStart");
-    this.fetchData();
-  }
 };
 </script>
 
 <style lang="scss">
-.text-center{
-  text-align: center;
+.text-center {
+    text-align: center;
 }
 </style>
